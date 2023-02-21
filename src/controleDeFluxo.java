11afcceb40ccd7317c81d4038c1e87cf3aa77711
Emanoel Faria <emanoelvisali@gmail.com>
2023-02-20 21:50:06 -0300

    Projeto Final Contador pt2

--- src/controleDeFluxo.java
+++ src/controleDeFluxo.java
@@ -1,30 +1,24 @@
 import java.util.Scanner;
 
-public class controleDeFluxo {
-    public static void main(String[] args) throws Exception {
+public class ControleDeFluxo {
+    public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         int primeiroParametro = 0;
         int segundoParametro = 0;
         try {
-
             System.out.println("Informe o primeiro parametro");
             primeiroParametro = entrada.nextInt();
-
             System.out.println("Informe o segundo parametro");
             segundoParametro = entrada.nextInt();
             entrada.close();
-            if (primeiroParametro > segundoParametro) {
-                for (int i = 0; i < primeiroParametro; i++) {
-                    System.out.println(i);
-
-                }
-            } else {
-                throw new parameterException();
+            if (!(primeiroParametro > segundoParametro)) {
+                throw new ParameterException("Nao corresponde ao solicitado, segundo parametro maior que o primeiro");
             }
-        } catch (parameterException e) {
-
-            System.out.println("Nao corresponde ao solicitado, segundo parametro maior que o primeiro");
+            for (int i = 0; i < primeiroParametro; i++) {
+                System.out.println(i);
+            }
+        } catch (ParameterException e) {
+            System.out.println(e.getMessage());
         }
-
     }
 }
--- src/parameterException.java
+++ src/parameterException.java
@@ -1,8 +1,5 @@
-public class parameterException extends Exception {
-    public static void main(String[] args) throws Exception {
-    
-        System.out.println("Segundo parametro maior que o primeiro, argumento inválido.");
-        System.out.println("Parametro Invalido por favor forneceça algum valor valido.");
- 
-}
+public class ParameterException extends Exception {
+    public ParameterException(String message) {
+        super(message);
+    }
 }
